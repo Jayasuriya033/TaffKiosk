dbee36f358d30cd617fbdf7354bd50fffe1532a4
Jayasuriya G <95901720+Jayasuriya033@users.noreply.github.com>
2024-07-07 11:55:53 +0530

    Update router.js

--- Backend/routes/router.js
+++ Backend/routes/router.js
@@ -1,1 +1,11 @@
 
+import express from 'express';
+import roleRouter from './role.js';
+import employeeRouter from './employee.js';
+
+const router = express.Router();
+
+router.use('/roles', roleRouter);
+router.use('/employees', employeeRouter);
+
+export default router;
