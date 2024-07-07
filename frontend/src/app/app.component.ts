4a28fa9ae0c7da6a5a49f42e7e45908b4b2fee0f
Jayasuriya G <95901720+Jayasuriya033@users.noreply.github.com>
2024-07-07 08:33:39 +0530

    Update app.component.ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-<<<<<<< HEAD
   title = 'frontend';
-=======
-  title = 'first-project';
->>>>>>> bb1bb1f787f5ce8683ce3cac7fc4e83c63e9c7e2
 }
