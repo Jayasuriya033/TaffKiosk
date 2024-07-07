abb06f0f59dfd3ed5ac4cd5e97a77f8b6d5fb5c4
Jayasuriya G <95901720+Jayasuriya033@users.noreply.github.com>
2024-07-07 08:34:33 +0530

    Update app.component.spec.ts

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -20,27 +20,21 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-<<<<<<< HEAD
   it(`should have as title 'frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('frontend');
-=======
   it(`should have as title 'first-project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('first-project');
->>>>>>> bb1bb1f787f5ce8683ce3cac7fc4e83c63e9c7e2
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-<<<<<<< HEAD
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend');
-=======
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, first-project');
->>>>>>> bb1bb1f787f5ce8683ce3cac7fc4e83c63e9c7e2
   });
 });
