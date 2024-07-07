359c5bed5afc55fb1e767c508cab901b0784699b
Jayasuriya G <95901720+Jayasuriya033@users.noreply.github.com>
2024-07-07 08:36:08 +0530

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-=======
-// import { NgModule } from '@angular/core';
-// import { RouterModule, Routes } from '@angular/router';
-// import { LoginComponent } from './login/login.component';
-// import { NotFoundComponent } from './not-found/not-found.component';
-
-// const routes: Routes = [
-//   {path:"", component: LoginComponent},
-//   {path:"login", component: LoginComponent},
-
-//   {path:"notfound", component: NotFoundComponent},
-//   {path:"**", redirectTo:"notfound"}
-// ];
-
-// @NgModule({
-//   imports: [RouterModule.forRoot(routes)],
-//   exports: [RouterModule]
-// })
-// export class AppRoutingModule { }
-
-
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
@@ -38,7 +12,6 @@ const routes: Routes = [
   { path: 'signup', component: SignupComponent },
   { path: '**', component: NotFoundComponent }
 ];
->>>>>>> bb1bb1f787f5ce8683ce3cac7fc4e83c63e9c7e2
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
